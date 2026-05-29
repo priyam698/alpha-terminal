970f277d77c8e93ec2ba604b19e99c95fd8365a7
priyam698 <priyamrana069@gmail.com>
2026-05-30 01:18:30 +0530

    fix: change serverless export syntax to commonjs format

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     // Enable CORS headers so your frontend can communicate securely
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -87,4 +87,4 @@ export default async function handler(req, res) {
     } catch (err) {
         return res.status(500).json({ error: 'Internal Gateway Routing Timeout Fault.' });
     }
-}
+};
